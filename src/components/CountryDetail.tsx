bbf602fd52c3ab89a05ad94086fd323906bb8507
Steven Kelmelis <steve.kelmelis@gmail.com>
2020-04-11 15:17:14 -0400

    fix: simplify modal styles

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -3,29 +3,26 @@ import React from 'react';
 import { Entry } from '../types/Entry';
 import { makeStyles } from '@material-ui/core';
 
-const modalStyle = {
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)'
-};
-
 const useStyles = makeStyles((theme) => ({
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
     color: theme.palette.text.primary,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3)
-  }
+    padding: theme.spacing(2, 4, 3),
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+  },
 }));
 
 export default function CountryDetail({ entry }: { entry: Entry }) {
   const classes = useStyles();
 
   return (
-    <div style={modalStyle} className={classes.paper}>
+    <div className={classes.paper}>
       <h2>{entry.country} Details</h2>
       <div>
         <div>Cases: {entry.cases}</div>
