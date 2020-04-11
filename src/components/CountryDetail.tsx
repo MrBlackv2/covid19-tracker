a8d106704a53c15afca14e19baeec953207bb05c
Steven Kelmelis <steve.kelmelis@gmail.com>
2020-04-11 12:36:48 -0400

    fix: country details modal

--- src/components/CountryDetail.tsx
+++ src/components/CountryDetail.tsx
@@ -29,16 +29,16 @@ export default function CountryDetail({ entry }: { entry: Entry }) {
       <h2>{entry.country} Details</h2>
       <div>
         <div>Cases: {entry.cases}</div>
-        <div>Cases (Today): {entry.cases}</div>
-        <div>Deaths: {entry.cases}</div>
-        <div>Deaths (Today): {entry.cases}</div>
-        <div>Recovered: {entry.cases}</div>
-        <div>Active: {entry.cases}</div>
-        <div>Critical: {entry.cases}</div>
-        <div>Cases per Million: {entry.cases}</div>
-        <div>Deaths per Million: {entry.cases}</div>
-        <div>Tests: {entry.cases}</div>
-        <div>Tests per Million: {entry.cases}</div>
+        <div>Cases (Today): {entry.todayCases}</div>
+        <div>Deaths: {entry.deaths}</div>
+        <div>Deaths (Today): {entry.todayDeaths}</div>
+        <div>Recovered: {entry.recovered}</div>
+        <div>Active: {entry.active}</div>
+        <div>Critical: {entry.critical}</div>
+        <div>Cases per Million: {entry.casesPerOneMillion}</div>
+        <div>Deaths per Million: {entry.deathsPerOneMillion}</div>
+        <div>Tests: {entry.tests}</div>
+        <div>Tests per Million: {entry.testsPerOneMillion}</div>
       </div>
     </div>
   );
