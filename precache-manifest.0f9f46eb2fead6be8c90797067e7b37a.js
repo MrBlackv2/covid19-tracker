df5d8aa2934a35c4d399688728dcef8f25a6aa4e
Steven Kelmelis <steve.kelmelis@gmail.com>
2020-04-19 22:41:39 -0400

    Updates

---
+++ precache-manifest.0f9f46eb2fead6be8c90797067e7b37a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27215276bdf5de58ae157005b0125949",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "29c6d0c33c7b09337329",
+    "url": "/covid19-tracker/static/css/main.eb01f12a.chunk.css"
+  },
+  {
+    "revision": "82e7d00b9da64991a1d9",
+    "url": "/covid19-tracker/static/js/2.2d89fa18.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/covid19-tracker/static/js/2.2d89fa18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29c6d0c33c7b09337329",
+    "url": "/covid19-tracker/static/js/main.02bce219.chunk.js"
+  },
+  {
+    "revision": "eb5ff0e51105f2f9eece",
+    "url": "/covid19-tracker/static/js/runtime-main.782e1f46.js"
+  }
+]);
